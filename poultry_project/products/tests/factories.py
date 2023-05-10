673d7552d9f16ff676590d2daa9820c5330bdf19
clydmaz <clydmaz@gmail.com>
2023-05-10 22:54:32 +0200

    used product factory in unit test

--- poultry_project/products/tests/factories.py
+++ poultry_project/products/tests/factories.py
@@ -9,11 +9,11 @@ class ProductFactory(factory.django.DjangoModelFactory):
     
     product_id = factory.fuzzy.FuzzyText()
     product_name = factory.fuzzy.FuzzyText()
-    description =factory.Faker('paragraph', nb_sentences=3, variable_nb_sentence = True)
-    unit_price = factory.fuzzy.FuzzyFloat()
-    quantity_in_stock = factory.fuzzy.FuzzyInteger()
+    description =factory.Faker('paragraph', nb_sentences=3)
+    unit_price = factory.fuzzy.FuzzyFloat(low=3.6, high=8.0, precision=2)
+    quantity_in_stock = factory.fuzzy.FuzzyInteger(low=0)
     unit_of_measure = factory.fuzzy.FuzzyText()
     slug = factory.LazyAttribute(lambda obj: slugify(obj.product_name))
 
-    class meta:
+    class Meta:
         model = Product
--- poultry_project/products/tests/test_models.py
+++ poultry_project/products/tests/test_models.py
@@ -4,17 +4,10 @@ pytestmark = pytest.mark.django_db
 
 from .. models import Product
 
-
+from . factories import ProductFactory
 
 def test___str__():
-    product = Product.objects.create(
-        product_id = "cpl0001",
-        product_name = "goose",
-        description = "duck like fowl with long neck",
-        unit_price = 0.1,
-        quantity_in_stock = 7,
-        unit_of_measure = "unit"
-    )
-
+    product = ProductFactory(product_name = "goose")
+    
     assert product.__str__() == "goose"
     assert str(product) == "goose"
