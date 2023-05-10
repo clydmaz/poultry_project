933f30508abd73f314a546dab843c7a88f5386f9
clydmaz <clydmaz@gmail.com>
2023-05-10 23:53:37 +0200

    added a created by field to product model

--- poultry_project/products/models.py
+++ poultry_project/products/models.py
@@ -1,4 +1,4 @@
-
+from django.conf import settings
 from django.urls import reverse
 from django.db import models
 from model_utils.models import TimeStampedModel
@@ -12,6 +12,11 @@ class Product(TimeStampedModel):
     quantity_in_stock = models.IntegerField("Units In Stock",default=0)
     unit_of_measure = models.CharField("Unit Of Measurement", max_length=4)
     slug = AutoSlugField("product_address", unique=True, always_update = False, populate_from="product_id")
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete = models.SET_NULL
+    )
 
     def __str__(self):
         return self.product_name
